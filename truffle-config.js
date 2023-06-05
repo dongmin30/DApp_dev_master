55bb42ab469d0306aff346bc09f1f3a1a5f3c418
Dongmin_Kim <dongmin0355@gmail.com>
2023-06-05 18:00:15 +0900

    migrations 작성 0.1

--- truffle-config.js
+++ truffle-config.js
@@ -1,22 +1,22 @@
-require('babel-regester');
+require('babel-register');
 require('babel-polyfill');
 
 module.exports = {
-  networks:{
-    development:{
-      host: '127.0.0.1',
-      port: '7545',
-      network_id: '*' // Connect to any network
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
     },
   },
   contracts_directory: './src/contracts/',
-  contracts_build_directory: './src/truffle_abis',
+  contracts_build_directory: './src/truffle_abis/',
   compilers: {
     solc: {
-      version: '^0.5.0', 
+      version: '^0.5.0',
       optimizer: {
         enabled: true,
-        runs: 200,
+        runs: 200
       },
     }
   }
